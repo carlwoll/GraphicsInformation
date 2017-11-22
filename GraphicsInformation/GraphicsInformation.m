ecf9b096e48623786345fe7f2097b9cc72f63ae4
Carl Woll <carlw@wolfram.com>
2017-11-22 08:57:32 -0800

    Initial commit

---
+++ GraphicsInformation/GraphicsInformation.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Nov 22, 2017 *)
+
+BeginPackage["GraphicsInformation`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2017.11.22*)
+
+Paclet[
+  Name -> "GraphicsInformation",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
