5ecda97bce224abf00c89933a56b8c01a12f1ed7
Carl Woll <carlw@wolfram.com>
2017-11-22 13:08:31 -0800

    Add initial GraphicsInformation.m file, and paclet support

--- GraphicsInformation/GraphicsInformation.m
+++ GraphicsInformation/GraphicsInformation.m
@@ -5,10 +5,81 @@
 BeginPackage["GraphicsInformation`"]
 (* Exported symbols added here with SymbolName::usage *) 
 
+GraphicsInformation::usage = "GraphicsInformation[graphics] returns sizing information about the graphics object"
+
 Begin["`Private`"]
 (* Implementation of the package *)
 
+GraphicsInformation[gr_Graphics] := Replace[
+    graphicsInformation[{gr}],
+    Rule[a_, {b_}] :> a -> b,
+    {1}
+]
 
+GraphicsInformation[gr:{__Graphics}] := Module[{info, res},
+    info = Flatten @ Reap[
+        Rule @@@ ReplaceAll[
+            "Regions",
+            FrontEndExecute @ ExportPacket[
+                toNotebook[gr],
+                "BoundingBox",
+                Verbose->True
+            ]
+        ],
+        _,
+        #1->#2[[1]]&
+    ];
+    res = extract[info] /@ Range @ Length @ gr;
+    Thread @ Rule[
+        {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange"},
+        Thread @ ReplaceAll[
+            {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange"},
+            res
+        ]
+    ]
+]
+
+toNotebook[gr_] := Notebook[
+    {
+    Cell[BoxData @ ToBoxes @ instrumentGraphics[gr],
+        "Output"
+    ]
+    },
+    WindowSize -> CurrentValue[EvaluationNotebook[], WindowSize],
+    Evaluator -> CurrentValue[EvaluationNotebook[], Evaluator]
+]
+
+instrumentGraphics[gr:{__Graphics}] := MapThread[
+    Show[#1,
+        GridLines -> {sowRange["X" -> #2], sowRange["Y" -> #2]},
+        Epilog -> {
+            Annotation[
+                Rectangle[Scaled[{0,0}], Scaled[{1,1}]],
+                "PlotRange", #2
+            ],
+            Annotation[
+                Rectangle[ImageScaled[{0,0}], ImageScaled[{1,1}]],
+                "ImageSize", #2
+            ]
+        }
+    ]&,
+    {gr, Range@Length@gr}
+]
+
+instrumentGraphics[gr_Graphics] := instrumentGraphics[{gr}]
+
+sowRange[label_] := Function[Sow[{##}, label]; None]
+
+extract[rules_][k_] := Module[{pr, is, xr, yr},
+    {pr, is, xr, yr} = {{"PlotRange",k}, {"ImageSize",k}, "X"->k, "Y"->k} /. rules;
+    {
+    "ImagePadding"->Abs[is-pr],
+    "ImageSize"->Abs[Subtract@@@is],
+    "PlotRangeSize"->Abs[Subtract@@@pr],
+    "ImagePaddingSize"->Total[Abs[is-pr],{2}],
+    "PlotRange"->{xr,yr}
+    }
+]
 
 End[]
 
--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,18 @@
 (* Paclet Info File *)
 
-(* created 2017.11.22*)
+(* created 2017/11/22*)
 
 Paclet[
-  Name -> "GraphicsInformation",
-  Version -> "0.0.1",
-  MathematicaVersion -> "6+",
-  Extensions -> {
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "GraphicsInformation",
+    Version -> "0.0.1",
+    MathematicaVersion -> "10+",
+    Description -> "GraphicsInformation[graphics] returns sizing information about the graphics object",
+    Creator -> "Carl Woll",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}, 
+            {"Kernel", Root -> ".", Context -> "GraphicsInformation`"}
+        }
+]
+
+
