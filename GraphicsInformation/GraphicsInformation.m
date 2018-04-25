6dbd3a8d7af4aa868f0ed6773c37f43806fca3a9
Carl Woll <carlw@wolfram.com>
2018-04-24 18:30:03 -0700

    Add BasePlotRange output

--- GraphicsInformation/GraphicsInformation.m
+++ GraphicsInformation/GraphicsInformation.m
@@ -31,9 +31,9 @@ GraphicsInformation[gr:{__Graphics}] := Module[{info, res},
     ];
     res = extract[info] /@ Range @ Length @ gr;
     Thread @ Rule[
-        {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange"},
+        {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange", "BasePlotRange"},
         Thread @ ReplaceAll[
-            {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange"},
+            {"ImagePadding", "ImageSize", "PlotRangeSize", "ImagePaddingSize", "PlotRange", "BasePlotRange"},
             res
         ]
     ]
@@ -45,35 +45,45 @@ toNotebook[gr_] := Notebook[
     Evaluator -> CurrentValue[EvaluationNotebook[], Evaluator]
 ]
 
-instrumentGraphics[gr:{__Graphics}] := MapThread[
-    Show[#1,
-        GridLines -> {sowRange["X" -> #2], sowRange["Y" -> #2]},
-        Epilog -> {
-            Annotation[
-                Rectangle[Scaled[{0,0}], Scaled[{1,1}]],
-                "PlotRange", #2
-            ],
-            Annotation[
-                Rectangle[ImageScaled[{0,0}], ImageScaled[{1,1}]],
-                "ImageSize", #2
-            ]
-        }
-    ]&,
-    {gr, Range@Length@gr}
+instrumentGraphics[gr:{__Graphics}] := Join[
+	MapThread[
+	    Show[#1,
+	        GridLines -> {sowRange["X" -> #2], sowRange["Y" -> #2]},
+	        Epilog -> {
+	            Annotation[
+	                Rectangle[Scaled[{0,0}], Scaled[{1,1}]],
+	                "PlotRange", #2
+	            ],
+	            Annotation[
+	                Rectangle[ImageScaled[{0,0}], ImageScaled[{1,1}]],
+	                "ImageSize", #2
+	            ]
+	        }
+	    ]&,
+	    {gr, Range@Length@gr}
+	],
+	MapThread[
+		Show[#,
+			PlotRangePadding->0,
+	        GridLines -> {sowRange["BX" -> #2], sowRange["BY" -> #2]}
+	    ]&,
+	    {gr, Range@Length@gr}
+	]
 ]
 
 instrumentGraphics[gr_Graphics] := instrumentGraphics[{gr}]
 
 sowRange[label_] := Function[Sow[{##}, label]; None]
 
-extract[rules_][k_] := Module[{pr, is, xr, yr},
-    {pr, is, xr, yr} = {{"PlotRange",k}, {"ImageSize",k}, "X"->k, "Y"->k} /. rules;
+extract[rules_][k_] := Module[{pr, is, xr, yr, uxr, uyr},
+    {pr, is, xr, yr, uxr, uyr} = {{"PlotRange",k}, {"ImageSize",k}, "X"->k, "Y"->k, "BX"->k, "BY"->k} /. rules;
     {
     "ImagePadding"->Abs[is-pr],
     "ImageSize"->Abs[Subtract@@@is],
     "PlotRangeSize"->Abs[Subtract@@@pr],
     "ImagePaddingSize"->Total[Abs[is-pr],{2}],
-    "PlotRange"->{xr,yr}
+    "PlotRange"->{xr,yr},
+    "BasePlotRange"->{uxr, uyr}
     }
 ]
 
